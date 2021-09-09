54984f9f416c6de79ab9d9f60db667db5ab22e08
Laderlappen <elmervermeulen@gmail.com>
2021-09-09 05:34:15 -0400

    Use orange warning color if the user has no voting power and dis play a Delegation Required message

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,29 @@
 import React, { Component } from "react";
 import "../layout/components/header.sass";
-
+//0x7E1d0353063F01CfFa92f4a9C8A100cFE37d8264
+//0x0000000000000000000000000000000000000000
 class Header extends Component {
   render() {
     return (
       <section className="header">
         <h1 className="header__title">Foundry Governance Proposals</h1>
-        <p className="header__balance">
-          {`Voting Power: ${
-            this.props.votingPower !== 0
-              ? ((this.props.votingPower / this.props.totalSupply) * 100)
-                  .toFixed(2)
-                  .toString() + "%"
-              : "0%"
-          }`}
+        <p className="header__balance__white">
+          {"Voting Power:"}
+          <text
+            className={
+              this.props.votingPower === "0"
+                ? "header__balance__orange"
+                : "header__balance__white"
+            }
+          >
+            {` ${
+              this.props.votingPower !== "0" && this.props.votingPower !== 0
+                ? ((this.props.votingPower / this.props.totalSupply) * 100)
+                    .toFixed(2)
+                    .toString() + "%"
+                : "0% (Delegation Required)"
+            }`}
+          </text>
           <br />
           {`Token Balance: ${this.props.balance}`}
           <br />
