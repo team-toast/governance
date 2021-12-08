fd39dd9f14c51a709db02e88a90db9e098be48e1
christopherbecker <>
2021-12-08 11:55:20 +0200

    [feature/PD-255] Progress - max widht of component

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -5,62 +5,70 @@ import "../layout/components/progressbar.sass";
 class ProgressBar extends Component {
   render() {
     return (
-      <div id="progressBar" className="flex">
-        <div className="process-name text-center">
-          <img src="/converting-icon.svg" />
-          <br />
-          <br />
-          Converting
+      <div id="progressBar">
+        <div className="max-width flex">
+          <div className="process-name text-center">
+            <img src="/converting-icon.svg" />
+            <br />
+            <br />
+            Converting
+          </div>
+          {this.props.processStage.length < 4 ? (
+            <div className="flex steps">
+              <div
+                className={
+                  this.props.processStage.length >= 1 ? "done" : "none"
+                }
+              >
+                <div className="text-center">
+                  <div className="progress-circle"></div>
+                  Approved
+                </div>
+              </div>
+              <div
+                className={
+                  this.props.processStage.length >= 2 ? "done" : "none"
+                }
+              >
+                <div className="text-center">
+                  <div className="progress-circle"></div>
+                  {this.props.firetext}
+                </div>
+              </div>
+              <div
+                className={
+                  this.props.processStage.length >= 3 ? "done" : "none"
+                }
+              >
+                <div className="text-center">
+                  <div className="progress-circle"></div>
+                  Complete
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className="flex steps">
+              <div className="busy">
+                <div className="text-center">
+                  <div className="progress-circle"></div>
+                  Approved
+                </div>
+              </div>
+              <div className="busy">
+                <div className="text-center">
+                  <div className="progress-circle"></div>
+                  {this.props.firetext}
+                </div>
+              </div>
+              <div className="done error">
+                <div className="text-center">
+                  <div className="progress-circle"></div>
+                  Could not complete. Please try again.
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-        {this.props.processStage.length < 4 ? (
-          <div className="flex steps">
-            <div
-              className={this.props.processStage.length >= 1 ? "done" : "none"}
-            >
-              <div className="text-center">
-                <div className="progress-circle"></div>
-                Approved
-              </div>
-            </div>
-            <div
-              className={this.props.processStage.length >= 2 ? "done" : "none"}
-            >
-              <div className="text-center">
-                <div className="progress-circle"></div>
-                {this.props.firetext}
-              </div>
-            </div>
-            <div
-              className={this.props.processStage.length >= 3 ? "done" : "none"}
-            >
-              <div className="text-center">
-                <div className="progress-circle"></div>
-                Complete
-              </div>
-            </div>
-          </div>
-        ) : (
-          <div className="flex steps">
-            <div className="busy">
-              <div className="text-center">
-                <div className="progress-circle"></div>
-                Approved
-              </div>
-            </div>
-            <div className="busy">
-              <div className="text-center">
-                <div className="progress-circle"></div>
-                {this.props.firetext}
-              </div>
-            </div>
-            <div className="done error">
-              <div className="text-center">
-                <div className="progress-circle"></div>
-                Could not complete. Please try again.
-              </div>
-            </div>
-          </div>
-        )}
         {this.props.processStage.length === 3 ||
           (this.props.processStage.length === 4 && (
             <button onClick={() => this.props.setStatus("", false)}>X</button>
